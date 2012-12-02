9a6dc777b52d6e19ab773913e555e64e76daa81a
Jimmy Larsson <jimmy.k.larsson@gmail.com>
2012-12-02 12:11:38 +0100

    correct docstring format

--- src/storage/io.clj
+++ src/storage/io.clj
@@ -45,50 +45,60 @@
 (defn seek [in pos]
   (seek-to in pos))
 
-(defn hexdump [barray]
+(defn hexdump 
   "Create a hex-string from a byte array"
+  [barray]
   (apply str (map #(with-out-str (printf "%02x" %)) barray)))
 
-(defn hexread [s]
+(defn hexread
   "Convert hex-string to a seekable input stream"
+  [s] 
   (java.nio.ByteBuffer/wrap 
     (byte-array 
       (map #(byte (Integer/parseInt % 16)) 
            (map #(apply str %) 
                 (partition-all 2 s))))))
 
-(defn node [children]
+(defn node
   "A HAMT node with children"
+  [children] 
   {:arcs (vector children)})
 
-(defn leaf [key, value]
+(defn leaf
   "A HAMT leaf node, with a key and a value"
+  [key, value] 
   {:arcs [] :key key :value value})
 
-(defn leaf? [node]
+(defn leaf?
   "Return true if node is a leaf"
+  [node] 
   (empty? (:arcs node)))
 
-(defn marshal-int [i]
+(defn marshal-int
   "Turns a 32-bit int into a 4 byte array (assumes big endian)"
+  [i]
   (.array (.putInt (java.nio.ByteBuffer/allocate 4) i)))
 
-(defn unmarshal-int [barray]
+(defn unmarshal-int
   "Turns a 32-bit int into a 4 byte array (assumes big endian)"
+  [barray]
   (.getInt (java.nio.ByteBuffer/wrap barray) 0))
 
-(defn marshal-string [s]
+(defn marshal-string
   "Turn string into byte sequence for disk storage"
+  [s] 
   (let [data (.getBytes s "utf-8")]
     (concat (marshal-int (count data)) data)))
 
-(defn unmarshal-string [in]
+(defn unmarshal-string
   "Read a string"
+  [in] 
   (let [len (unmarshal-int (read-bytes in 4))]
     (String. (read-bytes in len) "utf-8")))
 
-(defn marshal-node [node offset]
+(defn marshal-node
   "Create bytes from node, offset is added to all pointers"
+  [node offset] 
   (if (leaf? node)
     (concat (marshal-string (:key node)) 
             (marshal-string (:value node))
@@ -96,11 +106,12 @@
             (marshal-int 0))
     ()))
 
-(defn unmarshal-node [in position]
+(defn unmarshal-node
   "Read node from bytes"
+  [in position] 
   (seek in position)
-  (let [pointer (unmarshal-int (read-bytes in 4))
-        arcbits (unmarshal-int (read-bytes in 4))]
-    (seek in pointer)
-    {:key (unmarshal-string in) :value (unmarshal-string in) :arcs []}))
+    (let [pointer (unmarshal-int (read-bytes in 4))
+          arcbits (unmarshal-int (read-bytes in 4))]
+      (seek in pointer)
+      {:key (unmarshal-string in) :value (unmarshal-string in) :arcs []}))
 
