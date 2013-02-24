c3bdf7a1ecb4f90c31c8cf0f16a2851923785d0a
Jimmy Larsson <jimmy.k.larsson@gmail.com>
2013-02-24 08:04:12 +0100

    remove debug statements

--- src/storage/core.clj
+++ src/storage/core.clj
@@ -48,9 +48,7 @@
       (recur (+ 1 depth) (conj branch (io/node :depth depth)))
       (conj
         branch
-        (let [new-node (io/set-arc (io/node :depth depth) (nth leaf-hashes depth) (:pos leaf))]
-          #_(print (str "Modified node, arcno " (nth leaf-hashes depth) ", pos " (:pos leaf) ", leaf-hashes " (vec leaf-hashes) ", node: " new-node "\n"))
-          new-node)))))
+        (io/set-arc (io/node :depth depth) (nth leaf-hashes depth) (:pos leaf))))))
 
 (defn store
   "Store a key with a value, copying needed nodes, creating a new root and storing a new root pointer"
@@ -67,12 +65,10 @@
                     (if (= key (:key first-node))
                       (rest branch)
                       (grow-branch branch (hash-codes (:key first-node)) hashes))
-                    branch)
-          new-leaf (io/leaf key value)]
+                    branch)]
 
       ; Write the new leaf
-      #_(print (str "Writing new leaf to " (io/end-pointer db) ", hashes " (vec hashes) ", " new-leaf "\n"))
-      (io/write-bytes db (io/marshal-node new-leaf (io/end-pointer db)))
+      (io/write-bytes db (io/marshal-node (io/leaf key value) (io/end-pointer db)))
 
       ; Write the grown branch, deepest node first, modifying the arc pointers for the branch processed
       (dorun
@@ -80,9 +76,7 @@
           (io/write-bytes
             db
             (io/marshal-node
-              (let [node (io/set-arc node (nth hashes (:depth node)) (- (io/end-pointer db) (io/node-size)))]
-                #_(print (str "Writing key " key ", hash " (vec hashes) ", node " node "\n"))
-                node)
+              (io/set-arc node (nth hashes (:depth node)) (- (io/end-pointer db) (io/node-size)))
               (io/end-pointer db))))))
 
     (io/set-root-node db (- (io/end-pointer db) (io/node-size)))
@@ -92,11 +86,8 @@
   "Fetch a value for a key, returns nil if not found"
   ([key] (fetch *db* key))
   ([db key]
-    (let [hash (hash-codes key)
-          path (node-path db (io/root-node db) hash 0)
-          node (first path)]
-      #_(print (str "Fetching " key ", hash " (vec hash) ", path " path "\n"))
-      (cond 
+    (let [node (first (node-path db (io/root-node db) (hash-codes key) 0))]
+      (cond
         (nil? node) nil
         (= (name key) (:key node)) (:value node)
         :else nil))))
