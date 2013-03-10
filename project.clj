a484cc0c38aad3af215bb11a65f2d365b75ca429
Jimmy Larsson <jimmy.k.larsson@gmail.com>
2013-03-10 11:06:04 +0100

    bumped to clojure 1.5

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/jimlar/mnemosyne"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [aleph "0.3.0-beta13"]]
   :main mnemosyne.server
   :profiles {
